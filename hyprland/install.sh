8b9cad67b688187b8ab1b4f56c6ee505a1a2d491
Michael Stämpfli <michael.staempfli@gmail.com>
2024-12-13 22:51:26 +0100

    Add hyprland config

---
+++ hyprland/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+BASEDIR=$(cd $(dirname $BASH_SOURCE) && pwd)
+cd $BASEDIR
+
+source ../utils.sh
+
+link_file ./monitors.conf ~/.config/hypr/monitors.conf
+link_file ./look-and-feel.conf ~/.config/hypr/look-and-feel.conf
+link_file ./layout.conf ~/.config/hypr/layout.conf
+link_file ./input.conf ~/.config/hypr/input.conf
+link_file ./keybindings.conf ~/.config/hypr/keybindings.conf
+link_file ./rules.conf ~/.config/hypr/rules.conf
+link_file ./hyprland.conf ~/.config/hypr/hyprland.conf
