017cfeb0670be87986d61b19da9001fa76ca7ed7
Michael Stämpfli <michael.staempfli@gmail.com>
2024-12-23 08:09:00 +0100

    Function to mount USB devices

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -23,3 +23,13 @@ alias dcx='docker compose exec'
 
 # kubernetes
 alias k='kubectl'
+
+# USB
+usbmount() {
+  sudo mkdir -p /mnt/usbstick
+  sudo mount -o gid=users,fmask=113,dmask=002 /dev/sda /mnt/usbstick
+}
+
+usbumount() {
+  sudo umount /mnt/usbstick
+}
