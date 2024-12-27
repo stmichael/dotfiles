2510030b28d8c87a4c0947f2e17d4a3a70201d20
Michael Stämpfli <michael.staempfli@gmail.com>
2024-12-27 20:39:13 +0100

    Fix print queue display

--- waybar/modules/cups.sh
+++ waybar/modules/cups.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 queue=$(lpq -a | tail +2)
-tooltip=$(echo "$queue" | sed 'N;s/\n/\\n/')
+if [[ $queue = "" ]]; then
+  echo "{\"text\":\"0\", \"tooltip\":\"\"}"
+else
+  tooltip=$(echo "$queue" | sed 'N;s/\n/\\n/')
 
-echo "{\"text\":\"$(echo "$queue" | wc -l)\", \"tooltip\":\"$(echo "$tooltip")\"}"
+  echo "{\"text\":\"$(echo "$queue" | wc -l)\", \"tooltip\":\"$(echo "$tooltip")\"}"
+fi
