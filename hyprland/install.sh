6dbb3fad496751c19072f30c93776ebc8741f682
Michael Stämpfli <michael.staempfli@gmail.com>
2024-12-28 15:54:32 +0100

    Improved workspace distribution

--- hyprland/install.sh
+++ hyprland/install.sh
@@ -13,5 +13,3 @@ link_file ./input.conf ~/.config/hypr/input.conf
 link_file ./keybindings.conf ~/.config/hypr/keybindings.conf
 link_file ./rules.conf ~/.config/hypr/rules.conf
 link_file ./hyprland.conf ~/.config/hypr/hyprland.conf
-link_file ./monitors ~/.config/hypr/monitors
-link_file ~/.config/hypr/monitors/laptop-only.conf ~/.config/hypr/monitors.conf
--- zsh/modules/monitors.zsh
+++ zsh/modules/monitors.zsh
@@ -1,20 +1,25 @@
 monitor-setup-laptop() {
-  local name=$1
-  rm ~/.config/hypr/monitors.conf
-  ln -sf ~/.config/hypr/monitors/laptop-only.conf ~/.config/hypr/monitors.conf
-  hyprctl reload
+  # Move workspaces to monitors
+  hyprctl dispatch moveworkspacetomonitor 1 desc:Chimei Innolux Corporation 0x145A
+  hyprctl dispatch moveworkspacetomonitor 2 desc:Chimei Innolux Corporation 0x145A
+  hyprctl dispatch moveworkspacetomonitor 3 desc:Chimei Innolux Corporation 0x145A
+  hyprctl dispatch moveworkspacetomonitor 4 desc:Chimei Innolux Corporation 0x145A
+  # Update workspacerules so that waybar detects the assignments
+  hyprctl keyword workspace 1, desc:Chimei Innolux Corporation 0x145A
+  hyprctl keyword workspace 2, desc:Chimei Innolux Corporation 0x145A
+  hyprctl keyword workspace 3, desc:Chimei Innolux Corporation 0x145A
+  hyprctl keyword workspace 4, desc:Chimei Innolux Corporation 0x145A
 }
 
 monitor-setup-home() {
-  local name=$1
-  rm ~/.config/hypr/monitors.conf
-  ln -sf ~/.config/hypr/monitors/home-dual.conf ~/.config/hypr/monitors.conf
-  hyprctl reload
-}
-
-monitor-setup-custom() {
-  local name=$1
-  rm ~/.config/hypr/monitors.conf
-  ln -sf ~/.config/hypr/monitors/custom.conf ~/.config/hypr/monitors.conf
-  hyprctl reload
+  # Move workspaces to monitors
+  hyprctl dispatch moveworkspacetomonitor 1 desc:Chimei Innolux Corporation 0x145A
+  hyprctl dispatch moveworkspacetomonitor 2 desc:BNQ BenQ PD2710QC RAJ0256801Q
+  hyprctl dispatch moveworkspacetomonitor 3 desc:BNQ BenQ PD2710QC RAJ0256801Q
+  hyprctl dispatch moveworkspacetomonitor 4 desc:BNQ BenQ PD2710QC RAJ0256801Q
+  # Update workspacerules so that waybar detects the assignments
+  hyprctl keyword workspace 1, desc:Chimei Innolux Corporation 0x145A
+  hyprctl keyword workspace 2, desc:BNQ BenQ PD2710QC RAJ0256801Q
+  hyprctl keyword workspace 3, desc:BNQ BenQ PD2710QC RAJ0256801Q
+  hyprctl keyword workspace 4, desc:BNQ BenQ PD2710QC RAJ0256801Q
 }
