e9f924a949a225e6510b910c9498e5c7b9dde361
Michael Stämpfli <michael.staempfli@gmail.com>
2025-01-10 15:57:43 +0100

    Prevent disabling a single monitor

--- hyprland/scripts/monitor.sh
+++ hyprland/scripts/monitor.sh
@@ -13,6 +13,17 @@ enable_monitor() {
 
 disable_monitor() {
   local monitor=$1
+
+  if [[ $SAFE -eq 1 ]]
+  then
+    local numberOfMonitors=$(hyprctl monitors -j | jq -r '. | length')
+    if [[ $numberOfMonitors -le 1 ]]
+    then
+      echo "Only one monitor is left. Disabling it is dangerous!"
+      exit 1
+    fi
+  fi
+
   if [[ $DRY_RUN -eq 1 ]]
   then
     echo "-> hyprctl keyword monitor $monitor, disable"
@@ -29,10 +40,12 @@ usage() {
     echo "Options:"
     echo " -h, --help This help"
     echo " --dry-run  Doesn't perform any monitor operations, only prints what it would do"
+    echo " --safe     Doesn't disable a monitor if it's the last of its kind"
     exit 1
 }
 
 DRY_RUN=0
+SAFE=0
 while [[ $# -gt 0 ]]; do
   case $1 in
     -h|--help)
@@ -41,6 +54,10 @@ while [[ $# -gt 0 ]]; do
       ;;
     --dry-run)
       DRY_RUN=1
+      shift
+      ;;
+    --safe)
+      SAFE=1
       shift
       ;;
     *)
