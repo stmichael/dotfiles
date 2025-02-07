8318d72153db37f732a55be9b961fb32a9c24292
Michael Stämpfli <michael.staempfli@gmail.com>
2025-02-07 10:11:58 +0100

    Add better NAS mount points

--- zsh/modules/nas.zsh
+++ zsh/modules/nas.zsh
@@ -1,8 +1,20 @@
-nas-mount() {
-  sudo mount --mkdir -t nfs -o vers=4 192.168.0.20:/volume1/homes/stmichael /mnt/nas
-  echo "NAS available at /mnt/nas"
+HOME_MOUNT_POINT=/var/run/media/$USER/home
+SHARED_MOUNT_POINT=/var/run/media/$USER/Gemeinsam
+
+nas-mount-home() {
+  sudo mount --mkdir -t nfs -o vers=4 HueNAS:/volume1/homes/stmichael $HOME_MOUNT_POINT
+  echo "NAS available at $HOME_MOUNT_POINT"
 }
 
-nas-umount() {
-  sudo umount /mnt/nas
+nas-umount-home() {
+  sudo umount $HOME_MOUNT_POINT
+}
+
+nas-mount-shared() {
+  sudo mount --mkdir -t nfs -o vers=4 HueNAS:/volume1/Gemeinsam $SHARED_MOUNT_POINT
+  echo "NAS available at $SHARED_MOUNT_POINT"
+}
+
+nas-umount-shared() {
+  sudo umount $SHARED_MOUNT_POINT
 }
