d8448675a7190e684cd1dbbd5d6e7a40bb6d3166
Michael Stämpfli <michael.staempfli@gmail.com>
2025-02-07 11:18:15 +0100

    Use manual workspace layout change

--- waybar/install.sh
+++ waybar/install.sh
@@ -11,4 +11,5 @@ source ../utils.sh
 link_file ./config.jsonc ~/.config/waybar/config.jsonc
 link_file ./style.css ~/.config/waybar/style.css
 link_file ./modules/power-menu.xml ~/.config/waybar/modules/power-menu.xml
+link_file ./modules/workspaces-menu.xml ~/.config/waybar/modules/workspaces-menu.xml
 link_file ./modules/cups.sh ~/.config/waybar/modules/cups.sh
