017a3fbca42dc72f06af4dbc2b35ea47fd193f90
Michael Stämpfli <michael.staempfli@gmail.com>
2025-02-22 22:14:44 +0100

    Add catpuccin to yazi

--- yazi/install.sh
+++ yazi/install.sh
@@ -10,3 +10,4 @@ source ../utils.sh
 link_file ./yazi.toml ~/.config/yazi/yazi.toml
 link_file ./keymap.toml ~/.config/yazi/keymap.toml
 link_file ./init.lua ~/.config/yazi/init.lua
+download_file https://github.com/catppuccin/yazi/raw/refs/heads/main/themes/mocha/catppuccin-mocha-lavender.toml ~/.config/yazi/theme.toml
