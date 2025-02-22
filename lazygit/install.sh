2900137944d11f64f25639b0a1154db31903d849
Michael Stämpfli <michael.staempfli@gmail.com>
2025-02-22 21:55:11 +0100

    Add catpuccin to lazygit

--- lazygit/install.sh
+++ lazygit/install.sh
@@ -8,3 +8,4 @@ cd $BASEDIR
 source ../utils.sh
 
 link_file ./config.yml ~/.config/lazygit/config.yml
+download_file https://github.com/catppuccin/lazygit/raw/refs/heads/main/themes-mergable/mocha/lavender.yml ~/.config/lazygit/mocha-lavender.yml
