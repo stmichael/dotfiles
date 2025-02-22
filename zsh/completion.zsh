079910de3609a985943a28904e7ebc298e66e757
Michael Stämpfli <michael.staempfli@gmail.com>
2025-02-22 21:28:35 +0100

    Add catpuccin to fzf

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -79,5 +79,12 @@ export FZF_COMPLETION_OPTS='--border --info=inline'
 export FZF_COMPLETION_PATH_OPTS='--walker file,dir,follow,hidden'
 # Options for directory completion (e.g. cd **<TAB>)
 export FZF_COMPLETION_DIR_OPTS='--walker dir,follow'
+# Load catpuccin colors
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
 # Load fzf
 eval "$(fzf --zsh)"
