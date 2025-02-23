59eef5120848fc73f43da3ce4fb58a6a18d724d2
Michael Stämpfli <michael.staempfli@gmail.com>
2025-02-23 15:54:35 +0100

    Add catpuccin to rofi

--- rofi/install.sh
+++ rofi/install.sh
@@ -10,3 +10,4 @@ source ../utils.sh
 link_file ./config.rasi ~/.config/rofi/config.rasi
 link_file ./theme.rasi ~/.config/rofi/theme.rasi
 link_file ./scripts ~/.config/rofi/scripts
+download_file https://github.com/catppuccin/rofi/raw/refs/heads/main/themes/catppuccin-mocha.rasi ~/.config/rofi/catppuccin-mocha.rasi
