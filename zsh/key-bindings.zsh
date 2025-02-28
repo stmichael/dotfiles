37ca468d2f0db38de57e0a97c630740a50ea5b55
Michael Stämpfli <michael.staempfli@gmail.com>
2025-02-28 09:12:10 +0100

    Fix home and end keys in alacritty

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -1,6 +1,6 @@
 # Cursor movement
-bindkey "^[[H" beginning-of-line
-bindkey "^[[F" end-of-line
+bindkey "^[[1~" beginning-of-line
+bindkey "^[[4~" end-of-line
 
 # Text modification
 bindkey "^[[3~" delete-char
