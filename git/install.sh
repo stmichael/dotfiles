6ec1b022f44a200a251651107990f4be116f6720
Michael Stämpfli <michael.staempfli@gmail.com>
2025-04-16 14:53:09 +0200

    Add customer specific git config

--- git/install.sh
+++ git/install.sh
@@ -8,3 +8,6 @@ cd $BASEDIR
 source ../utils.sh
 
 link_file ./gitconfig ~/.gitconfig
+link_file ./gitconfig-private ~/.gitconfig-private
+link_file ./gitconfig-spoud ~/.gitconfig-spoud
+link_file ./gitconfig-finnova ~/.gitconfig-finnova
