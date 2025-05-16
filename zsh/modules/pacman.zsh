4dc60feed20b69bfdfb2b198d5a3d03b48a8ba13
Michael Stämpfli <michael.staempfli@gmail.com>
2025-05-16 10:05:27 +0200

    Auto-answer yay questions

--- zsh/modules/pacman.zsh
+++ zsh/modules/pacman.zsh
@@ -1,5 +1,5 @@
 alias pac-update='sudo pacman -Syu'
-alias yay-update='yay -Syu'
+alias yay-update='yay -Syu --noconfirm --answerdiff=None --answeredit=None'
 
 firmware-update() {
   fwupdmgr refresh
