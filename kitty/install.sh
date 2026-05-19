089a8456c16c679859edcaf27522efd6aecf5e81
Michael Stämpfli <michael.staempfli@gmail.com>
2026-05-19 17:55:56 +0200

    Add matugen theme to kitty

--- kitty/install.sh
+++ kitty/install.sh
@@ -8,4 +8,3 @@ cd $BASEDIR
 source ../utils.sh
 
 link_file ./kitty.conf ~/.config/kitty/kitty.conf
-download_file https://raw.githubusercontent.com/catppuccin/kitty/main/themes/mocha.conf ~/.config/kitty/catppuccin-mocha.conf
