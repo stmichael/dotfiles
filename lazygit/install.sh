9c2069ef556c41c4f1b5d9ab594c948b110a7864
Michael Stämpfli <michael.staempfli@gmail.com>
2026-05-20 16:21:12 +0200

    Add matugen theme to zsh

--- lazygit/install.sh
+++ lazygit/install.sh
@@ -8,4 +8,3 @@ cd $BASEDIR
 source ../utils.sh
 
 link_file ./config.yml ~/.config/lazygit/config.yml
-download_file https://github.com/catppuccin/lazygit/raw/refs/heads/main/themes-mergable/mocha/lavender.yml ~/.config/lazygit/mocha-lavender.yml
